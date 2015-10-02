72ff4ae7453fcd0276a055881652b6eaef6d5d4c
Aloulen ZAkariae <zakriae.al@gmail.com>
2015-10-02 17:49:03 +0200

    add fichier main

---
+++ src/aloulen/Main.groovy
@@ -0,0 +1,12 @@
+package src.aloulen
+
+class Main {
+    static void main(def args) {
+        Main m = new Main()
+        println m.returnHello()
+    }
+
+    String returnHello() {
+        'Hello'
+    }
+}
